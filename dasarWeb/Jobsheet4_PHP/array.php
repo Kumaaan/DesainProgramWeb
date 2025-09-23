c2b179c01745ebd673b6d1eb3341fc54e8b1b5d7
Your Name <luhungadi571@gmail.com>
2025-09-23 07:42:18 +0700

    Memnambahkan kode program untuk menampilkan nilai diatas rata-rata

--- dasarWeb/Jobsheet4_PHP/array.php
+++ dasarWeb/Jobsheet4_PHP/array.php
@@ -54,4 +54,30 @@ echo "<br><br>Daftar nilai mahasiswa dalam mata kuliah $matakuliah: <br>";
 foreach ($daftarNilai [$matakuliah] as $nilai) {
     echo "Nama: {$nilai [0]}, Nilai: {$nilai [1]} <br>";
 }
+
+
+
+$daftarNilai = [
+    ['Alice', 85],
+    ['Bob', 92],
+    ['Charlie', 78],
+    ['David', 64],
+    ['Eva', 90],
+];
+
+$total = 0;
+foreach ($daftarNilai as $siswa) {
+    $total += $siswa[1]; 
+}
+
+$rataRata = $total / count($daftarNilai);
+
+echo "<br><br><br>Rata-rata nilai kelas: $rataRata<br><br>";
+
+echo "Daftar siswa dengan nilai di atas rata-rata:<br>";
+foreach ($daftarNilai as $siswa) {
+    if ($siswa[1] > $rataRata) {
+        echo "Nama: {$siswa[0]}, Nilai: {$siswa[1]}<br>";
+    }
+}
 ?>
