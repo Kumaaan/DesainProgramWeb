e850c9f827973beed943b06aa8b8ab52fdc38eba
Your Name <luhungadi571@gmail.com>
2025-09-29 14:28:50 +0700

    Mencoba menggunakan function

---
+++ dasarWeb/JS05_PHP2/function.php
@@ -0,0 +1,16 @@
+<?php
+function perkenalan ($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan ("Hamdana", "Hallo");
+
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan($saya, $ucapanSalam);
+?>
