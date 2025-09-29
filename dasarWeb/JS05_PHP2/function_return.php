8244cce977e8496b97713c0cd24655f011bb8e3d
Your Name <luhungadi571@gmail.com>
2025-09-29 14:53:33 +0700

    Memanggil function didalam function lain

--- dasarWeb/JS05_PHP2/function_return.php
+++ dasarWeb/JS05_PHP2/function_return.php
@@ -4,5 +4,12 @@ function hitungUmur($thn_lahir, $thn_sekarang) {
     return $umur;
 }
 
-echo "Umur saya adalah ". hitungUmur (2005, 2025). " tahun" 
+function perkenalan ($nama, $salam="Assalamualaikum") {
+    echo $salam.",";
+    echo " Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Saya berusia ". hitungUmur (2005, 2025) . " tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+perkenalan ("Elok");
 ?>
