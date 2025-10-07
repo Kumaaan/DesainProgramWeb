10c5770e7939d1ce5ce182006fda4b33db2046f1
Your Name <luhungadi571@gmail.com>
2025-10-07 23:07:56 +0700

    jquery UI

---
+++ dasarWeb/praktik_jquery/jqueryUI.js
@@ -0,0 +1,10 @@
+- <html>
+
+    <head>
+        <script src="jquery-3.7.1.js"></script>
+        <script src="jquery-ui-1.12.1.custom/jquery-ui.js"></script>
+    </head>
+
+    <body>
+    </body>
+</html>
